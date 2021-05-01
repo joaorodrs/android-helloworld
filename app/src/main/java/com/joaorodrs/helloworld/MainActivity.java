94afeaf184f81936b2fb8cfb79673c8c66ccc9c7
João Paulo <jpjoao1001@gmail.com>
2021-05-01 15:01:39 -0300

    text message with button switcher added

--- app/src/main/java/com/joaorodrs/helloworld/MainActivity.java
+++ app/src/main/java/com/joaorodrs/helloworld/MainActivity.java
@@ -3,6 +3,9 @@ package com.joaorodrs.helloworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onBtnClick(View view) {
+        TextView txtHello = findViewById(R.id.txtMessage);
+        txtHello.setText("something else");
     }
 }
